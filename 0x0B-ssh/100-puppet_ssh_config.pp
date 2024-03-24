ca95f62d17b30b306a8e66e16ae4f157c3516a23
oladakinjoye <oladakinjoye@gmail.com>
2024-03-23 17:22:16 -0700

    create puppet 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,9 @@
 #!usr/bin/env bash 
 #using puppet to make changes to our configuration file 
 #file { 'ect/ssh/ssh_cofig ':
-	ensure=>present
+	ensure=>present,
 	content=>"
+
 	#SSH client configuration
 	host*
 	IdentityFile ~/.ssh/school
