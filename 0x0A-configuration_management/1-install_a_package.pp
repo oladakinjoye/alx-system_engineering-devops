98ebc6c51b8175b6681bead09f7c1d8a01574215
oladakinjoye <oladakinjoye@gmail.com>
2024-03-26 15:10:47 -0700

    update install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-#!/usr/bin/pup
-# This code is to install a version of flask (2.1.0)
+# This code is to install special version of flask (2.1.0)
 	package {'flask':
 	 ensure   => '2.1.0',
 	 provider => 'pip3',
